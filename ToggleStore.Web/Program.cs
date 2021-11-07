62cd1ad485631fcab7349b554270d1ea5c406216
Eduardo Aguiar <eduaguiar281@hotmail.com>
2021-11-07 20:51:58 -0300

    Adicionando Azure AppConfiguration

--- ToggleStore.Web/Program.cs
+++ ToggleStore.Web/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ToggleStore.Web
 {
@@ -19,8 +14,12 @@ namespace ToggleStore.Web
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder.ConfigureAppConfiguration(config =>
+                    {
+                        var settings = config.Build();
+                        var connection = settings.GetConnectionString("AppConfig");
+                        config.AddAzureAppConfiguration(options =>
+                            options.Connect(connection).UseFeatureFlags());
+                    }).UseStartup<Startup>());
     }
 }
